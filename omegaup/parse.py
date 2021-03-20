2422081901b2f6c791ab8578e419f9f5ef19ba81
JosepHyv <josephynimoto@gmail.com>
2021-03-19 23:45:12 -0600

    added new code

---
+++ omegaup/parse.py
@@ -0,0 +1,28 @@
+from html.parser import HTMLParser
+from urllib.request import urlopen
+
+class OmegaupProblemParse(HTMLParser):
+	'''
+	def __init__(self, folder):
+		HTMLParser.__init__(self)
+	'''
+	def handle_starttag(self, tag, attrs):
+		print("Encountered a start tag:", tag)
+
+	def handle_endtag(self, tag):
+		print("Encountered an end tag :", tag)
+
+	def handle_data(self, data):
+		return data
+		print("Encountered some data  :", data)
+
+test = OmegaupProblemParse()
+file = input()
+html = urlopen(file).read()
+test.feed(html.decode('utf-8'))
+
+algo = test.handle_data(html.decode("utf-8"))
+
+some = algo.split()
+print( *some )
+print(type(algo))
